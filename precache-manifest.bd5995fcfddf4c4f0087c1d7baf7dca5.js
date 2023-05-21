22558aed9c3593f4d527483c5ce8b8444e383783
maksvty <yurij1101@gmail.com>
2023-05-22 00:13:59 +0300

    Updates

---
+++ precache-manifest.bd5995fcfddf4c4f0087c1d7baf7dca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6f95ba68133846dac54",
+    "url": "/pwa/static/css/main.8d252d74.chunk.css"
+  },
+  {
+    "revision": "c6f95ba68133846dac54",
+    "url": "/pwa/static/js/main.c6f95ba6.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/pwa/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "3e4e159ee829cb8b574d",
+    "url": "/pwa/static/js/2.3e4e159e.chunk.js"
+  },
+  {
+    "revision": "3602666b63ff1ac2ced0",
+    "url": "/pwa/static/js/runtime~main.3602666b.js"
+  },
+  {
+    "revision": "3160fd7685966950a4a4d7c5718b9be3",
+    "url": "/pwa/index.html"
+  }
+];
