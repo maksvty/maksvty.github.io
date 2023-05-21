2c0dc884af065a6bc4956d1cc2a0ffbe379747ef
maksvty <yurij1101@gmail.com>
2023-05-22 01:00:14 +0300

    Updates

--- precache-manifest.ab2e22ca4870bb5aaff5500bc0a165f4.js
+++ precache-manifest.ab2e22ca4870bb5aaff5500bc0a165f4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "922ae532576bfb926594",
+    "revision": "b45560330cec701d720c",
     "url": "/pwa/static/css/main.8d252d74.chunk.css"
   },
   {
-    "revision": "922ae532576bfb926594",
-    "url": "/pwa/static/js/main.922ae532.chunk.js"
+    "revision": "b45560330cec701d720c",
+    "url": "/pwa/static/js/main.b4556033.chunk.js"
   },
   {
     "revision": "071f6974c376b039043f",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/pwa/static/js/runtime~main.3602666b.js"
   },
   {
-    "revision": "f1d59d666b5d127d8ce32e8738923bcc",
+    "revision": "c15ed4b28f75fc2d242946716fb6b3ea",
     "url": "/pwa/index.html"
   }
 ];
