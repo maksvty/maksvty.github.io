119813c8c1f82760a55b18131024e45211617e3c
maksvty <yurij1101@gmail.com>
2023-05-23 17:15:44 +0300

    Updates

---
+++ precache-manifest.c58f98dc606f7a6c592b83a190b226c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd92cdfefa56f057da47",
+    "url": "/static/css/main.8d252d74.chunk.css"
+  },
+  {
+    "revision": "dd92cdfefa56f057da47",
+    "url": "/static/js/main.dd92cdfe.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "3e4e159ee829cb8b574d",
+    "url": "/static/js/2.3e4e159e.chunk.js"
+  },
+  {
+    "revision": "fb573bd4075a37a7f80b",
+    "url": "/static/js/runtime~main.fb573bd4.js"
+  },
+  {
+    "revision": "fca72e2d02a82028391bedd582fc2777",
+    "url": "/index.html"
+  }
+];
